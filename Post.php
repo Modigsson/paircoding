134c2300219c10fa2184692489309297e293b305
edvwib <edde275@gmail.com>
2018-02-13 10:38:25 +0100

    hej

--- Post.php
+++ Post.php
@@ -5,16 +5,23 @@ declare(strict_types=1);
 
 class Post
 {
-  protected $title, $author;
+  protected $title, $author, $publishDate;
 
-  public function __construct(string $title, Author $author)
+  public function __construct(string $title, Author $author, DateTime $dt)
   {
     $this->title = $title;
     $this->author = $author;
+    $this->publishDate = $dt;
+
   }
 
-  public function getTitle():string
+  public function getTitle(): string
   {
     return $this->title;
+  }
+
+  public function getDateTime():string
+  {
+    return $this->publishDate->format('M d, Y ');
   }
 }
--- index.php
+++ index.php
@@ -7,18 +7,18 @@ require_once __DIR__.'/Post.php';
 $author = new Author('Arnold Schwarzenegger');
 
 $blog = new Blog([
-    new Post('Traveling Through Time', $author),
-    new Post('How to Successfully Fight the Resistance', $author),
+    new Post('Traveling Through Time', $author, new DateTime('1985-02-12')),
+    new Post('How to Successfully Fight the Resistance', $author, new DateTime('1991-09-12')),
 ]);
 
-$post = new Post('Smiling 101', $author);
+$post = new Post('Smiling 101', $author, new DateTime('2003-06-12'));
 
 $blog->publish($post);
 
 printf("There are %d posts published by %s:\n", count($blog->getPosts()), $author->getName());
 
 foreach ($blog->getPosts() as $post) {
-    printf("- %s\n", $post->getTitle());
+    printf("- %s - %s\n", $post->getTitle(), $post->getDateTime());
 }
 
 // There are 3 posts published by Arnold Schwarzenegger:
